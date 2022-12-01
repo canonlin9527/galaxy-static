3a4236ed53f0f3bbc945a5a33a90a115c65b07b1
canon.lin <canon.lin@vipshop.com>
2022-12-01 22:43:45 +0800

    add monaco editor

--- src/pages/Sugar/Module/index.tsx
+++ src/pages/Sugar/Module/index.tsx
@@ -7,21 +7,20 @@ import { useEffect } from 'react';
 import { sourceList } from '@/api/source';
 import { Button } from 'antd';
 import { getToken } from '@/utils/auth';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 const SugarModule: React.FC = () => {
   const { name, token } = useModel('global');
 
 
-
-
-
   return (
-    <PageContainer ghost>
-      <div className={styles.container}>
+    // <PageContainer ghost>
+    <div className={styles.container}>
 
-      </div>
-    </PageContainer>
+
+
+    </div>
+    // </PageContainer>
   );
 };
 
