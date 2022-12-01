4c464fb199eac1fb3c4d44405253516ff5d71b34
canon.lin <canon.lin@vipshop.com>
2022-12-01 23:00:41 +0800

    add monaco editor

--- src/pages/Sugar/Module/index.tsx
+++ src/pages/Sugar/Module/index.tsx
@@ -8,14 +8,31 @@ import { sourceList } from '@/api/source';
 import { Button } from 'antd';
 import { getToken } from '@/utils/auth';
 import Cookies from 'js-cookie';
+import { componentList } from '@/api/component';
 
 const SugarModule: React.FC = () => {
   const { name, token } = useModel('global');
+
+  const getCompList = async () => {
+    let res = await componentList();
+    console.log('component', res);
+  }
+
+
+  // @ts-ignore
+  useEffect(() => {
+    (async function () {
+      let res = await componentList();
+      console.log('component', res);
+    })()
+
+  },[])
 
 
   return (
     // <PageContainer ghost>
     <div className={styles.container}>
+      <Button type={'primary'} onClick={getCompList}>请求</Button>
 
 
 
